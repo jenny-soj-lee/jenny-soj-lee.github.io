e9e479deae319188c781615782674fcb67dbe2c7
Jenny Lee <jenny.soj.lee@gmail.com>
2025-08-02 21:28:08 -0700

    initial commit: setup

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,11 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
-  accentColor: "#1d4ed8",
+  name: "Jenny Lee",
+  title: "Hardware Design Engineer",
+  description: "Portfolio website of Jenny Lee",
+  accentColor: "#459814ff",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "jenny.soj.lee@gmail.com",
+    linkedin: "https://linkedin.com/in/jenny-soj-lee",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
